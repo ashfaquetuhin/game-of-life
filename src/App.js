2cbbc0e38a6d14d83379572ed8a69d2b309380a1
Ashfaque Mahmud <ashfaquetuhin@gmail.com>
2019-02-02 19:57:45 +0600

    third commit

--- src/App.js
+++ src/App.js
@@ -2,26 +2,107 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const tick = grid => {
+  const size = grid.length;
+  const newGrid = range(size).map(() => range(size));
+
+  const get = (i, j) => {
+    if (i < 0 || i >= size || j < 0 || j >= size) {
+      return 0;
+    } else {
+      return grid[i][j];
+    }
+  }
+
+  const neighbours = (i, j) => {
+    return (
+      get(i - 1, j - 1) +
+      get(i - 1, j) +
+      get(i - 1, j + 1) +
+
+      get(i, j - 1) +
+      get(i, j + 1) +
+
+      get(i + 1, j - 1) +
+      get(i + 1, j) +
+      get(i + 1, j + 1)
+    );
+  }
+
+  grid.forEach((row, i) => {
+    row.forEach((cell, j) => {
+      const n = neighbours(i, j);
+      newGrid[i][j] = grid[i][j];
+      if (grid[i][j] === 1) {
+        if (n <= 1 || n >= 4) {
+          newGrid[i][j] = 0;
+        }
+      } else {
+        if (n === 3) {
+          newGrid[i][j] = 1;
+        }
+      }
+    })
+  });
+  
+  return newGrid;
+}
 
 const range = n => Array(n).fill().map ((_, i) => i);
 
-const Cell = () => <div className="cell"> </div>;
+const Cell = ({state}) => (
+  <div
+   className="cell"
+   style={{
+    backgroundColor: state === 1 ? 'black' : 'white'
+   }}
+   />
+);
 
-const Row = ({size}) => (
+const Row = ({row}) => (
   <div className="row">
-  {range(size).map(j => <Cell key={j} />)}
+  {row.map((state, j) => <Cell key={j} state={state} />)}
   </div>
   );
-const Grid = ({size}) => (
+const Grid = ({grid}) => (
   <div className="grid">
-  {range (size).map(i => <Row key ={i} size={20} />)}
+  {grid.map((row, i) => <Row key ={i} row={row} />)}
   </div>
-
-  )
+ );
 class App extends Component {
+  state = {
+    grid: [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]
+
+  }
+
+  simulate() {
+    this.setState({
+      grid : tick (this.state.grid)
+    });
+  }
+
+  componentDidMount () {
+    setInterval(() => this.simulate(), 200);
+  }
   render () {
-    return <Grid size={20} />;
+    return <Grid grid={this.state.grid} />;
     }
   }
+
+
 
 export default App;
